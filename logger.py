1f14f1f047b46c0d84fc2d2520e003557f3d2591
mcscope <mcscope@gmail.com>
2015-01-16 10:24:12 -0800

    remove Phant stuff, update config, python 3 -> 2

--- logger.py
+++ logger.py
@@ -1,60 +1,60 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 import config
 
 import time
 from datetime import datetime
-
+import pytz
 import serial
-from phant import Phant
+logging_begin =  datetime.now(pytz.utc)
+print_timing = False
 
+def hack_log(data_to_log):
 
-def read_and_store(bean_serial, phant_endpoint):
-    print('Requesting...')
+    print str(data_to_log)
+
+def read_and_store(bean_serial):
     bean.write(b'\n')
     raw_data = bean.readline()
     data = raw_data.decode('utf-8').rstrip('\r\n')
 
     if len(data) == 0:
-        print('Bean timed out.\n')
+        print 'Bean timed out.\n'
         return False
 
     data_split = data.split('\t')
     if len(data_split) < len(config.DATA_FIELDS):
-        print('Expected %s fields, but only received %s from Bean.' %
-              (len(config.DATA_FIELDS), len(data_split)))
+        print 'Expected %s fields, but only received %s from Bean.' % \
+              (len(config.DATA_FIELDS), len(data_split))
         return False
 
-    print('    %s' % datetime.now())
-    for num, field in enumerate(config.DATA_FIELDS):
-        print('    %s: %s' % (field, data_split[num]))
+    labeled_data = dict(zip(config.DATA_FIELDS, data_split))
+    labeled_data['time'] = (datetime.now(pytz.utc) - logging_begin).total_seconds()
 
-    print('Sending...')
-    phant.log(*data_split)
-
-    print('Done!\n')
+    hack_log(labeled_data)
     return True
 
-print('Data destination: %s' % config.PUBLIC_KEY)
-print('Data fields: %s' % str(config.DATA_FIELDS))
-phant = Phant(config.PUBLIC_KEY, *config.DATA_FIELDS,
-              private_key=config.PRIVATE_KEY)
 bean = serial.Serial(config.BEAN_SERIAL_PATH,
                      timeout=config.BEAN_REQ_TIMEOUT_SECS)
-
 last_run = None
+delays = []
 
 while True:
-    now = datetime.now()
+    now = datetime.now(pytz.utc)
 
     if last_run:
         since_last_run = (now - last_run).total_seconds()
+        delays.append(since_last_run)
+
+    if delays and len(delays) % 10 == 0 and print_timing:  # log average delay, for troubleshooting
+        print sum(delays)/len(delays)
 
     if not last_run or since_last_run >= config.INTERVAL_SECS:
-        success = read_and_store(bean, phant)
+        success = read_and_store(bean)
         if success:
             last_run = now
 
     else:
         until_next_run = config.INTERVAL_SECS - since_last_run
+        print "TRIED TO READ FASTER THAN INTERVAL -- MUST SLEEP"
         time.sleep(until_next_run)
--- sample_config.py
+++ sample_config.py
@@ -1,5 +1,3 @@
-PUBLIC_KEY = 'roogbz2Oz4Fw3ZOLOW7o'
-PRIVATE_KEY = 'jkkYzyv6yBHnKAEdE5jo'
 DATA_FIELDS = ['batt', 'temp', 'a0', 'a1']
 
 INTERVAL_SECS = 3
